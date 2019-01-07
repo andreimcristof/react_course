1e44996b5e9a2defb1641eb850d69805137d44b4
andrei <andreimcristof@gmail.com>
2019-01-07 19:17:03 +0100

    __

--- seasons/seasons/src/SeasonDisplay.js
+++ seasons/seasons/src/SeasonDisplay.js
@@ -1,8 +1,35 @@
 import React from 'react';
+import './SeasonDisplay.css';
 
-const SeasonDisplay = () => {
+const seasonConfig = {
+    summer: {
+        text: 'Summer Season',
+        iconName: 'sun'
+    },
+    winter: {
+        text: 'Winter Season',
+        iconName: 'snowflake'
+    }
+}
+
+const getSeason = (lat, month) => {
+    if(month > 2 && month < 9) {
+        return lat > 0 ? 'summer' : 'winter'
+    } else {
+        return lat > 0 ? 'winter' : 'summer'
+    }
+}
+
+const SeasonDisplay = (props) => {
+    const season = getSeason(props.lat, new Date().getMonth())
+    const { text, iconName } = seasonConfig[season];
+    
     return (
-        <div>Season Display</div>
+        <div className={`season-display ${season}`}>
+            <i className={`${iconName} icon massive left`} />
+            <h1>{text}</h1>
+            <i className={`${iconName} icon massive right`} />
+        </div>
     );
 }
 export default SeasonDisplay;
--- seasons/seasons/src/index.js
+++ seasons/seasons/src/index.js
@@ -1,22 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
+    state = { lat: null, errorMsg: '' }
 
-        this.state = { lat: null }
-
-        window.navigator.geolocation.getCurrentPosition(
-            pos => {
-                this.setState({ lat: pos.coords.latitude });
-            }, 
-            err => console.error(err)
-        );
+    renderContent() {
+        if(this.state.errorMsg && !this.state.lat) {
+            return <div>Error: {this.state.errorMsg}</div>
+        }
+        if(!this.state.errorMsg && !this.state.lat) {
+            return <Spinner message="Please accept location request" />
+        }
+        if(!this.state.errorMsg && this.state.lat) {
+            return <SeasonDisplay lat={this.state.lat} />
+        }
     }
+    
     render(){
-        return <div>Latitude: {this.state.lat}</div>
+        return (
+            <div className="border red">
+                {this.renderContent()}
+            </div>
+        )
+    }
+
+    componentDidMount() {
+        this.fetchCoords();
+    }
+
+    fetchCoords() {
+        window.navigator.geolocation.getCurrentPosition(
+            pos => this.setState({ lat: pos.coords.latitude }), 
+            err => this.setState({ errorMsg: err.message })
+        );
     }
 }
 
